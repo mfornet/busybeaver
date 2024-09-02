b92966667baeee8ee499ee10f8354c15eae8590f
Thomas Vigouroux <me@vigoux.eu>
2024-09-02 12:51:17 +0200

    docs(closedset): explain closed set proof

--- Busybeaver/ClosedSet.lean
+++ Busybeaver/ClosedSet.lean
@@ -1,3 +1,12 @@
+/-
+This file contains the [Closed Set](https://wiki.bbchallenge.org/wiki/Closed_Set) proof technique.
+
+The main way to use it is through the `closed_set P` tactic, which replaces a goal of `¬M.halts C`
+by two goals:
+- proving that `P` is closed under "progressing" steps
+- proving that `C` eventually reaches an element of the set `P`
+
+-/
 import Busybeaver.Basic
 import Busybeaver.Reachability
 
@@ -6,24 +15,51 @@ open TM
 variable {M: Machine L S}
 
 structure ClosedSet (M: Machine L S) (base: Config L S → Prop) (I: Config L S) where
-  closed : ∀ (A: { S // base S }), ∃ (B: {S // base S}), A -[M]->+ B
+  closed : ∀ (A: {S // base S}), ∃ (B: {S // base S}), A -[M]->+ B
   enters : ∃ (N: {S // base S}), I -[M]->* N
 
 namespace ClosedSet
+
+def offset (closed: ClosedSet M p I) (hN: p N): ClosedSet M p N :=
+by use closed.closed, ⟨N, hN⟩, .refl
 
 lemma nonHalting (inst: ClosedSet M p I): ¬M.halts I := by {
   intro ⟨final, hFinal⟩
   induction final using Nat.caseStrongInductionOn generalizing I with
   | zero => {
+    /-
+    Assume that I is the final state.
+
+    Based on that, I itself is necessarily in the closed set (e.g. I is the state mentionned in
+    .enters). This is a contradiction because that would mean we can step from I.
+    -/
     obtain ⟨F, hFL, hFR⟩ := hFinal
     cases hFR
     obtain ⟨⟨N, pN⟩, hN⟩ := inst.enters
     have hIN := Machine.halts_in.evstep_same hFL hN
-    obtain ⟨⟨N', pN'⟩, hNN'⟩ := inst.closed ⟨N, pN⟩
-    simp_all
+    simp at hIN
+    cases hIN
+
+    -- Here, we know that I is in the closed set, we use the closure of the set to get a
+    -- contradiction
+
+    obtain ⟨_, hNN'⟩ := inst.closed ⟨I, pN⟩
     exact Machine.halts_in.no_progress hFL hNN'
   }
   | ind n IH => {
+    /-
+    Now assume we need to take a non-zero number of steps to stop, say n+1 steps.
+
+    We have:
+
+    I -[M]->* N -[M]->+ N' ...
+        ^ .enters
+                  ^ .closed
+
+    We can _offset_ the closed set instance to that N', we know we took at least one step too to get
+    to N'. Thus, by property of progression, we know that we stop in _at most_ n steps (the +1
+    cancels with the progression) to stop and the induction hypothesis applies.
+    -/
     obtain ⟨⟨N, pN⟩, hN⟩ := inst.enters
     obtain ⟨⟨N', pN'⟩, hNN'⟩ := inst.closed ⟨N, pN⟩
     simp_all
@@ -34,17 +70,15 @@ lemma nonHalting (inst: ClosedSet M p I): ¬M.halts I := by {
     obtain ⟨nfin, hnfin⟩ := hIN'.to_multistep
     have hnfinn := Machine.halts_in.within hFinal hnfin
     have hnfinHalts := Machine.halts_in.preceeds hFinal hnfin hnfinn
-    simp_all
-    refine IH _ ?_ ?_ hnfinHalts
+    simp [*] at hnfinn hnfinHalts
+
+    -- hnfinHalts : M halts in at most n steps from N', we can apply the induction hypothesis
+
+    refine IH _ ?_ (inst.offset pN') hnfinHalts
     · exact Nat.sub_le n nfin
-    constructor
-    · exact inst.closed
-    exists ⟨N', pN'⟩
-    simp_all
-    exact .refl
   }
 }
 
-/-- Proves non-termination using closed position set reasonning. -/
+/-- Proves non-termination using closed set reasonning. -/
 macro "closed_set" p:term : tactic =>
   `(tactic| suffices ClosedSet _ $p _ from this.nonHalting <;> constructor)
