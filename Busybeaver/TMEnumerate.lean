dc68fa64178456a7a1d96f68135e0e419d132f46
Thomas Vigouroux <me@vigoux.eu>
2024-09-02 12:51:09 +0200

    fix(halts_no_halt): update to new halts def

--- Busybeaver/TMEnumerate.lean
+++ Busybeaver/TMEnumerate.lean
@@ -7,15 +7,15 @@ variable {L S: ℕ} [Inhabited $ Label L] [Inhabited $ Symbol S]
 open TM
 
 lemma Machine.halts_of_no_halt {M: Machine L S} (h: ∀ (l: Label L) (s: Symbol S), M l s ≠ .halt):
-  ¬M.halts := by {
+  ¬M.halts init := by {
     intro hM
-    obtain ⟨⟨state, ⟨sym, _, _⟩⟩, ⟨hstf, _⟩⟩ := hM
-    simp [Machine.LastState, Machine.step] at hstf
-    split at hstf
+    obtain ⟨n, ⟨state, ⟨head, _, _⟩ ⟩, finLast, _⟩ := hM
+    simp [Machine.LastState, Machine.step] at finLast
+    split at finLast
     swap
-    · cases hstf
+    · cases finLast
     rename_i hstsy
-    exact absurd hstsy (h state sym)
+    exact absurd hstsy (h state head)
   }
 
 instance {M: Machine L S}: Decidable (∀ (l: Label L) (s: Symbol S), M l s ≠ .halt) := inferInstance
