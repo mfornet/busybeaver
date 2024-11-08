d7d90201c86adc1c1e049bf8367909af2d4c6f1c
Thomas Vigouroux <me@vigoux.eu>
2024-11-08 08:46:20 +0100

    feat: display machine in colors

--- bin/Beavui.lean
+++ bin/Beavui.lean
@@ -3,17 +3,15 @@ import Busybeaver
 
 open LCurses TM
 
-def ntoColor (n: ℕ): Color :=
-  #[Color.blue, Color.red, Color.green, Color.yellow, Color.magenta][n % 5]'(by {
-    simp
-    apply Nat.mod_lt
-    decide
-  })
+def colors := #[Color.blue, Color.red, Color.green, Color.yellow, Color.magenta]
 
 def main : List String → IO UInt32
 | [fst] => do
   let Except.ok { l, s, M } := Parse.pmachine.run fst | throw <| IO.userError "Failed to parse"
   let root ← initscr
+  start_colors
+
+  let pairs ← colors.mapM (λ fg ↦ new_pair (fg:=fg))
 
   let Mrep := toString <| repr M
   for state in List.finRange (l + 1) do
@@ -21,7 +19,13 @@ def main : List String → IO UInt32
       match M state sym with
       | .halt => root.addstr "---"
       | .next sym dir lab => do
-        
+        root.addstr s!"{toString sym}{repr dir}"
+        root.addch (Char.ofNat <| 'A'.toNat + lab) {pair:=pairs[↑lab % pairs.size]'(by {
+          apply Nat.mod_lt
+          sorry
+        })}
+    if state < l then
+      root.addch '_'
 
   let _ ← root.getch
 
